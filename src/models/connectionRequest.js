64988a400d137f4242d1cb9800c5d9090ec4e3d8
Mahesh <gmaheshreddy538@gmail.com>
2026-03-20 15:57:30 +0530

    refactor: enhance pre and post save hooks for connection requests

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -21,13 +21,18 @@ const connectionRequestSchema = new mongoose.Schema({
 
 connectionRequestSchema.index({ fromUserId: 1, toUserId: 1 });
 
+// pre hook to prevent users from sending connection requests to themselves
 connectionRequestSchema.pre('save', function () {
     console.log('Pre-save hook executed for ConnectionRequest');
     if (this.fromUserId.toString() === this.toUserId.toString()) {
         throw new Error("You cannot send a connection request to yourself")
     }
 });
-
+//post hook to log when a connection request is created
+connectionRequestSchema.post('save', function (doc) {
+    console.log(`Post-save hook executed for ConnectionRequest`);
+    console.log(`Connection request from ${doc.fromUserId} to ${doc.toUserId} has been created with status ${doc.status}`);
+});
 const ConnectionRequest = mongoose.model('ConnectionRequest', connectionRequestSchema);
 
 module.exports = ConnectionRequest;
