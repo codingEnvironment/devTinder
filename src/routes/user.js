16b1b7375510bba397ab1a58f9ab45505f9729c7
Mahesh <gmaheshreddy538@gmail.com>
2026-03-21 19:31:57 +0530

    fix: add missing ref to User model in connectionRequest schema and improve user data population in received requests endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const userAuth = require("../middleware/userAuth");
-const UserModel = require("../models/userModel");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const userData = "firstName lastName _id";
 
 // Get all received connection requests
 router.get("/user/requests/received", userAuth, async (req, res) => {
@@ -11,11 +11,12 @@ router.get("/user/requests/received", userAuth, async (req, res) => {
         const connectionRequests = await ConnectionRequestModel.find({
             toUserId,
             status: "interested"
-        });
+        }).populate('fromUserId', userData) // Populate the fromUserId field with user details
+        // .populate('toUserId', userData) // Optionally populate the toUserId field as well
 
         res.status(200).json({ 
             message: "Data fetched successfully",
-            data: connectionRequests 
+            data: connectionRequests
         });
     } catch (error) {
         res.status(500).json({ error: "Failed to fetch requests, Err: " + error.message });
